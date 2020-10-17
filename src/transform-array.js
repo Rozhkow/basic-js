8d32447f84af3fed987e924c5b59f5abaad42333
Rozhkow <70947620+Rozhkow@users.noreply.github.com>
2020-10-18 01:15:26 +0300

    feat: add code

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  let mass = []
+  
+  if(!Array.isArray(arr)) return ERROR
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  if (arr.length === 0) return []
+
+  for(let i = 0; i < arr.length; i++) 
+     if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' && arr[i] !== '--discard-next' && arr[i] !== '--discard-prev') 
+    mass.push(arr[i])
+   
+     else if (arr[i] === '--double-next' && arr[i + 1] !== undefined) 
+    mass.push(arr[i + 1])
+    
+     else if (arr[i] === '--double-prev' && arr[i - 1] !== undefined && arr[i-2] !== "--discard-next") 
+    mass.push(arr[i - 1])
+    
+     else if (arr[i] === '--discard-next' && arr[i + 1] !== undefined) 
+    i++
+
+     else if (arr[i] === '--discard-prev' && arr[i - 1] !== undefined && arr[i-2] !== "--discard-next") 
+    mass.pop(arr[i - 1])
+  
+  return mass
+}
